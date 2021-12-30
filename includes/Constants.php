3394124c98559d9e63e0c5f41d9e5fd4b04e0c25
GauravPPawar <45897859+GauravPPawar@users.noreply.github.com>
2021-12-31 04:47:49 +0530

    new constants added

--- includes/Constants.php
+++ includes/Constants.php
@@ -1,7 +1,7 @@
 <?php
 
 	define('DB_NAME', 'where is my place');
-	define('DB_USER', 'root');
-	define('DB_PASSWORD', '');
-	define('DB_HOST', 'localhost');
+	define('DB_USER', 'gauravpawar');
+	define('DB_PASSWORD', 'bYWbt8EDJYg8PKe');
+	define('DB_HOST', 'wimp.mysql.database.azure.com');
 ?>
